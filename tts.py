1c99bb950ce7bb440af9c57c598bea3140de4db9
Akshay Jadhav <starkt887@gmail.com>
2025-03-10 19:44:51 +0530

    updated the current mp3

--- tts.py
+++ tts.py
@@ -1,6 +1,5 @@
-# Import the required module for text to speech conversion
+
 from gtts import gTTS
-# This module is imported so that we can do some os operations
 import os
 
 
@@ -20,40 +19,23 @@ def empytVOFiles():
         print("emptied the folder")
     else:
         print("folder is empty")
-    # print(os.listdir("outputVOfiles"))'
 
 inputFiles=getAllFiles()
 if len(inputFiles)>0:
     empytVOFiles()
-    language = 'en'
-    accent="co.uk" #"us"
+    language = 'hi' #en
+    accent="co.in" #"us" "co.uk" "co.in"
     for id,inpFile in enumerate(inputFiles):
         try:
             f=open(f"inputtextfiles\\{inpFile}","r",encoding="utf-8")
             
-            # The text that you want to convert to audio
+
             fileText = f.read().strip()
             fileName=inpFile.split(".")[0]
             myobj = gTTS(text=fileText, lang=language,tld=accent, slow=False)
             myobj.save(f"outputVOfiles\\{fileName}.mp3")
-            # export to mp3
+       
             os.system(f"start outputVOfiles\\{fileName}.mp3")
         except:
             print("something went wrong!")
 
-# os.system("start outputVOfiles\\output.mp3")
-
-
-
-#Algorithm
-# text to speech setup and convert the speech in playable mp3 file
-# Passing the text and language to the engine, 
-# here we have marked slow=False. Which tells 
-# the module that the converted audio should 
-# have a high speed
-# Saving the converted audio in a mp3 file named
-# Playing the converted file
-
-
-# unused code
-# mytext=" ".join(mytext.split()) 
